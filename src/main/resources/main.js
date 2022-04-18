7679dfb1bbe9fc1ad353d35057f99d4d70ef93bd
TannerHartt <tannerhart24@yahoo.com>
2022-04-17 19:13:33 -0700

    Began work on designing a webpage with a custom landing page

--- src/main/resources/main.js
+++ src/main/resources/main.js
@@ -1,3 +1,10 @@
 function goToUrl( url) {
     window.location.href =url;
 }
+
+let toggleButton = document.querySelector(".container");
+
+toggleButton.onclick = function() {
+    toggleButton.classList.toggle("active")
+}
+
