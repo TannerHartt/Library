7f95bb5e6eed4eeb134435f38fc7d0708ba7bb13
TannerHartt <tannerhart24@yahoo.com>
2022-04-29 22:08:21 -0700

    Beginning work on another portfolio

---
+++ src/main/resources/AnotherPortfolio/folio.js
@@ -0,0 +1,25 @@
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId)
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        });
+    }
+}
+
+showMenu('nav-toggle','nav-menu');
+
+
+const navLink = document.querySelector('.nav_link');
+
+function linkAction() {
+    navLink.forEach(n => n.classList.remove('active'));
+    this.classList.add('active');
+
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
